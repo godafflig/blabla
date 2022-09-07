f2d216bf25644b19abedc2cb3295e3d5d4040e95
btom <tom.benat@ynov.com>
2022-09-08 00:32:17 +0200

    debug

--- main/main.go
+++ main/main.go
@@ -3,5 +3,5 @@ package main
 import "piscine"
 
 func main() {
-	piscine.PrintNbr(0)
+	piscine.PrintNbr(-9223372036854775808)
 }
--- printnbr.go
+++ printnbr.go
@@ -7,8 +7,27 @@ import (
 func PrintNbr(nb int) {
 	var i int = 1
 
-	if nb == 0 {
+	if nb == -9223372036854775808 {
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+		z01.PrintRune('2')
+		z01.PrintRune('2')
+		z01.PrintRune('3')
+		z01.PrintRune('3')
+		z01.PrintRune('7')
+		z01.PrintRune('2')
 		z01.PrintRune('0')
+		z01.PrintRune('3')
+		z01.PrintRune('6')
+		z01.PrintRune('8')
+		z01.PrintRune('5')
+		z01.PrintRune('4')
+		z01.PrintRune('7')
+		z01.PrintRune('7')
+		z01.PrintRune('5')
+		z01.PrintRune('8')
+		z01.PrintRune('0')
+		z01.PrintRune('8')
 		return
 	}
 	if nb < 0 {
