9ee1ea0f6a8e2420a6280b2fb1cf523c0570c578
btom <tom.benat@ynov.com>
2022-09-09 00:00:54 +0200

    debug

---
+++ isupper.go
@@ -0,0 +1,21 @@
+package piscine
+
+func IsUpper(s string) bool {
+	alphabet := [26]string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"}
+	var count int = 0
+	var i int
+	var j int
+	for i = 0; i < len(alphabet) -1; i++ {
+		for j = 0; j < len(s); j++ {
+			if string(alphabet[i]) == string(s[j]){
+				count ++
+				
+			}
+		}
+	}
+	if count == len(s) {
+	return true
+		} else { 
+		return false
+	}
+}
