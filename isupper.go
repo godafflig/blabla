432507a933b14633b9008883c71dfa51f8ae5649
btom <tom.benat@ynov.com>
2022-09-09 00:01:11 +0200

    debug

--- isupper.go
+++ isupper.go
@@ -5,17 +5,17 @@ func IsUpper(s string) bool {
 	var count int = 0
 	var i int
 	var j int
-	for i = 0; i < len(alphabet) -1; i++ {
+	for i = 0; i < len(alphabet)-1; i++ {
 		for j = 0; j < len(s); j++ {
-			if string(alphabet[i]) == string(s[j]){
-				count ++
-				
+			if string(alphabet[i]) == string(s[j]) {
+				count++
+
 			}
 		}
 	}
 	if count == len(s) {
-	return true
-		} else { 
+		return true
+	} else {
 		return false
 	}
 }
