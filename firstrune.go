24dc9c9a43346f928d48199c38e24d976fbae796
btom <tom.benat@ynov.com>
2022-09-08 12:58:21 +0200

    debug

--- firstrune.go
+++ firstrune.go
@@ -1,9 +1,6 @@
 package piscine
 
 func FirstRune(s string) rune {
-	if rune(s[0]) == 'â' {
-		return '♥'
-	}
-
+	
 	return rune(s[0])
 }
---
+++ lastrune.go
@@ -0,0 +1,7 @@
+package piscine
+
+func LastRune(s string) rune {
+	var a int
+	a = len(s) -1
+	return rune(s[a])
+}
