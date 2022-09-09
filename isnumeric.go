56bd44eb338040dcfe6365e0c2318ea019fcf452
btom <tom.benat@ynov.com>
2022-09-09 17:29:41 +0200

    debug

--- isnumeric.go
+++ isnumeric.go
@@ -3,11 +3,10 @@ package piscine
 func IsNumeric(s string) bool {
 	alphabet := [10]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	var count int = 0
-	var i int
-	var j int
+
 	for i = 0; i < len(s)-1; i++ {
 		for j = 0; j <= len(alphabet)-1; j++ {
-			if string(alphabet[j]) == string(s[i]) {
+			if rune(== string(s[i]) {
 				count++
 			}
 		}
